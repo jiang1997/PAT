ab5fbe873b2031f8fbadc29a3b4d1695c1a22f98
jiang1997 <jieke@live.cn>
2020-03-03 19:19:13 +0800

    lala

---
+++ Advanced/1018/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Node {
+    int numOfbikes;
+};
+
+int main() {
+    int matrix[505][505] = {0};
+    int C, N, S, M;
+    cin >> C >> N >> S >> M;
+    
+    return 0;
+}
